03c58de6c9cdec2ac6c7bda48fd806bfbec915f2
Benjamin Meyer <ben@meyerhome.net>
2002-02-13 12:20:29 +0000

    added comments
    svn path=/trunk/kdemultimedia/kaudiocreator/; revision=136305

--- job.h
+++ job.h
@@ -1,10 +1,22 @@
+/***************************************************************************
+                                job.h
+                             -------------------
+    begin                : Friday Feb 9 2002
+    copyright            : (C) 2001 by Benjamin Meyer
+    email                : ben-dev@meyerhome.net
+ ***************************************************************************/
+
 #ifndef JOB_H
 #define JOB_H
 
+/**
+ * The job class is what is passed around the system.  All of the data about
+ * the file beeing ripped and encoded is stored in here.
+ */
 class Job{
 
 public:
-  inline Job():id(-1),genre("other"),group(""),album(""),song(""), track(-1),year(-1),lastSongInAlbum(false){};
+  inline Job():id(-1),genre("Other"),group(""),album(""),song(""), track(-1),year(-1),lastSongInAlbum(false){};
   int id; 
   QString genre;		
   QString group;
@@ -12,7 +24,6 @@ public:
   QString song;
   int track;
   int year;
-  int length;
 
   QString location;
   QString newLocation;
