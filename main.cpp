0875afca17e54d2435f8bdd6d4d7be41f0723d5a
Benjamin Meyer <ben@meyerhome.net>
2002-02-13 13:14:47 +0000

    changed version and copyright.  removed unnecessary includes
    svn path=/trunk/kdemultimedia/kaudiocreator/; revision=136310

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
 /***************************************************************************
                                   main.cpp
                              -------------------
-    begin                : Sun Feb 13 01:05:24 EST 2000
-    copyright            : (C) 2000 by Benjamin Meyer
+    begin                : Sun Feb 13 EST 2002
+    copyright            : (C) 2002 by Benjamin Meyer
     email                : ben-dev@meyerhome.net
  ***************************************************************************/
 
 #include <kapplication.h>
 #include "kaudiocreator.h"
-
-//#include <string.h>
 #include <kcmdlineargs.h>
-
 #include <kaboutdata.h>
 #include <klocale.h>
-//#include <stdlib.h>
-//#include <stdio.h>
 
 int main(int argc, char *argv[]){
-
-  // about data
-  KAboutData aboutData("kaudiocreator", I18N_NOOP("KAudioCreator"), "0.1",
+  KAboutData aboutData("kaudiocreator", I18N_NOOP("KAudioCreator"), "0.5",
     I18N_NOOP("CD ripper and audio encoder frontend"), KAboutData::License_LGPL, "(c) 2002, Benjamin Meyer",
     0, "http://www.csh.rit.edu/~benjamin/");
     aboutData.addAuthor("Benjamin Meyer", I18N_NOOP("Original author"), "ben@meyerhome.net");
