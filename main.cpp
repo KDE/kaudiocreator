cc259620acdcf46eb38caae8e47ff2f7b572b7f7
Benjamin Meyer <ben@meyerhome.net>
2003-07-15 02:51:00 +0000

    CVS_IGNORE Updating code to match KDE Licensing Policy Part 1
    svn path=/trunk/kdemultimedia/kaudiocreator/; revision=237144

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-/***************************************************************************
-                                  main.cpp
-                             -------------------
-    begin                : Sun Feb 13 EST 2002
-    copyright            : (C) 2002 by Benjamin Meyer
-    email                : ben-dev@meyerhome.net
- ***************************************************************************/
+/**
+ * This file is part of the KAudioCreator package
+ * Copyright (C) 2003 Benjamin C Meyer (ben+kaudiocreator at meyerhome dot net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #include <kapplication.h>
 
@@ -16,7 +28,7 @@ int main(int argc, char *argv[]){
   KAboutData aboutData("kaudiocreator", I18N_NOOP("KAudioCreator"), "0.99",
     I18N_NOOP("CD ripper and audio encoder frontend"), KAboutData::License_LGPL, "(c) 2003, Benjamin Meyer",
     0, "http://www.csh.rit.edu/~benjamin/");
-    aboutData.addAuthor("Benjamin Meyer", I18N_NOOP("Original author"), "ben@meyerhome.net");
+    aboutData.addAuthor("Benjamin Meyer", I18N_NOOP("Original author"), "ben+kaudiocreator@meyerhome.net");
 
   // command line
   KCmdLineArgs::init(argc, argv, &aboutData);
--- tracksimp.h
+++ tracksimp.h
@@ -1,10 +1,22 @@
-/***************************************************************************
-                              tracksimp.h
-                             -------------------
-    begin                : Friday Feb 9 2002
-    copyright            : (C) 2001 by Benjamin Meyer
-    email                : ben-dev@meyerhome.net
- ***************************************************************************/
+/**
+ * This file is part of the KAudioCreator package
+ * Copyright (C) 2003 Benjamin C Meyer (ben+kaudiocreator at meyerhome dot net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #ifndef TRACKSIMP_H
 #define TRACKSIMP_H
