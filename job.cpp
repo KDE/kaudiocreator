e82f65af6f1d8add6414bb21acb13f189e181a26
Dirk Mueller <mueller@kde.org>
2005-09-06 17:05:00 +0000

    unbreak compilation
    svn path=/branches/KDE/3.3/kdemultimedia/kaudiocreator/; revision=457821

--- job.cpp
+++ job.cpp
@@ -24,7 +24,7 @@
  * A helper function to replace %X with the stuff in the album.
  * if quote is true then put "" around the %X
  */
-void Job::replaceSpecialChars(QString &str, bool quote, QMap<QString, QString> _map){
+QString Job::replaceSpecialChars(const QString &str, bool quote, QMap<QString, QString> _map){
   QMap<QString,QString> map = _map;
   
   map.insert("title", track_title);
@@ -38,10 +38,10 @@ void Job::replaceSpecialChars(QString &str, bool quote, QMap<QString, QString> _
   map.insert("albumcomment", comment);
   map.insert("albumartist", group);
   
-	if (quote)
-      str = KMacroExpander::expandMacrosShellQuote(str, map);
+  if (quote)
+      return KMacroExpander::expandMacrosShellQuote(str, map);
   else
-      str = KMacroExpander::expandMacros(str, map);
+      return KMacroExpander::expandMacros(str, map);
 }
 
 void Job::fix(const QString &in, const QString &out){
