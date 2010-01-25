8c4e883b84b911e28c5064985a0e38e2d1c4e8d9
Gerd Fleischer <gerdfleischer@web.de>
2010-01-25 15:47:32 +0000

    use FindPackageHandleStandardArgs for discid
    svn path=/trunk/extragear/multimedia/kaudiocreator/; revision=1080087

--- cmake/modules/FindDiscId.cmake
+++ cmake/modules/FindDiscId.cmake
@@ -1,28 +1,19 @@
-INCLUDE(UsePkgConfig)
-PKGCONFIG(libdiscid _DiscIdIncDir _DiscIdLinkDir _DiscIdLinkFlags _DiscIdCflags)
+# Module to find the discid library
+#
+# It defines
+#  DISCID_INCLUDE_DIR - the include dir
+#  DISCID_LIBRARIES - the required libraries
+#  DISCID_FOUND - true if both of the above have been found
 
-FIND_PATH(DISCID_INCLUDE_DIR discid/discid.h
-    ${_DiscIdIncDir}
-    /usr/include
-    /usr/local/include
-)
+# Copyright (c) 2006,2007 Laurent Montel, <montel@kde.org>
+# Copyright (c) 2010 Gerd Fleischer
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
 
-FIND_LIBRARY(DISCID_LIBRARIES discid
-    ${_DiscIdLinkDir}
-    /usr/lib
-    /usr/local/lib
-)
+FIND_PATH(DISCID_INCLUDE_DIR discid/discid.h)
 
-IF (DISCID_INCLUDE_DIR AND DISCID_LIBRARIES)
-    SET(DISCID_FOUND TRUE)
-ENDIF (DISCID_INCLUDE_DIR AND DISCID_LIBRARIES)
+FIND_LIBRARY(DISCID_LIBRARIES NAMES discid)
 
-IF (DISCID_FOUND)
-    IF (NOT DiscId_FIND_QUIETLY)
-	MESSAGE(STATUS "Found DiscId: ${DISCID_LIBRARIES}")
-    ENDIF (NOT DiscId_FIND_QUIETLY)
-ELSE (DISCID_FOUND)
-    IF (DiscId_FIND_REQUIRED)
-	MESSAGE(FATAL_ERROR "Could not find DiscId")
-    ENDIF (DiscId_FIND_REQUIRED)
-ENDIF (DISCID_FOUND)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args( DiscId DEFAULT_MSG
+                                   DISCID_INCLUDE_DIR DISCID_LIBRARIES)
